c715f5bd6840a955ec3cfc871f89f3b50b52f5a1
victoriavilleIT <83372520+victoriavilleIT@users.noreply.github.com>
2021-06-23 10:04:25 -0400

    Create SAPInstall.bat

---
+++ SAPInstall.bat
@@ -0,0 +1,6 @@
+START C:\Temp\SAP750\"SAP 750"\Setup\NwSapSetup.exe /product=SAPGUI /silent
+ping -n 60 127.0.0.1 >NUL 
+START C:\Temp\SAP750Patch12\"SAP 750 - Patch 12"\gui750_12-80001468.exe /Silent
+ping -n 60 127.0.0.1 >NUL
+START C:\Temp\SAP750Patchtext\"SAP Patch Text"\SCRLTESP00_0-80004046.EXE /Silent
+ping -n 60 127.0.0.1 >NUL
