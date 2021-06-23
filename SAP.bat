b23dd0b43965c516469813dea6043c656eb21550
victoriavilleIT <83372520+victoriavilleIT@users.noreply.github.com>
2021-06-23 10:16:05 -0400

    Update SAP.bat

--- SAP.bat
+++ SAP.bat
@@ -1,3 +1,4 @@
 powershell https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/DownloadSAP.ps1
+ping -n 30 127.0.0.1
 call https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/SAPInstall.bat
 
