6fca0b54c5c16e883a75289c83d925d8947bc874
victoriavilleIT <83372520+victoriavilleIT@users.noreply.github.com>
2021-06-25 08:20:54 -0400

    Update SAPInstall.bat

--- SAPInstall.bat
+++ SAPInstall.bat
@@ -1,3 +1,8 @@
+mkdir c:\temp
+powershell.exe -command "Invoke-WebRequest https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/DownloadSAP.ps1 -Outfile C:\Temp\sap.ps1"
+ping -n 30 127.0.0.1 >NUL
+powershell.exe -executionpolicy bypass -file C:\Temp\sap.ps1
+ping -n 60 127.0.0.1 >NUL
 START C:\Temp\SAP750\"SAP 750"\Setup\NwSapSetup.exe /product=SAPGUI /silent
 ping -n 60 127.0.0.1 >NUL 
 START C:\Temp\SAP750Patch12\"SAP 750 - Patch 12"\gui750_12-80001468.exe /Silent
