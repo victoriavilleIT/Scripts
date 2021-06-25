192d18a13f25c0905711b991ad3565234c5368ef
victoriavilleIT <83372520+victoriavilleIT@users.noreply.github.com>
2021-06-25 08:23:22 -0400

    Delete SAP.bat

--- SAP.bat
+++
@@ -1,4 +0,0 @@
-powershell.exe https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/DownloadSAP.ps1
-ping -n 30 127.0.0.1
-call https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/SAPInstall.bat
-
