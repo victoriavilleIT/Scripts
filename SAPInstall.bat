932619384cd0f8eab3aef79557947013a992e665
victoriavilleIT <83372520+victoriavilleIT@users.noreply.github.com>
2021-09-08 08:31:35 -0400

    Update SAPInstall.bat

--- SAPInstall.bat
+++ SAPInstall.bat
@@ -4,6 +4,4 @@ ping -n 60 127.0.0.1 >NUL
 powershell.exe -command "Invoke-WebRequest https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/SAPInstall2.bat -Outfile C:\Temp\SAPInstall2.bat"
 ping -n 30 127.0.0.1 >NUL
 powershell.exe -executionpolicy bypass -file C:\Temp\sap.ps1
-ping -n 30 127.0.0.1 >NUL
-cd C:\Temp
-SAPInstall2.bat
+
