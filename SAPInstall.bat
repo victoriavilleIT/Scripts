5ff0cf592d277094ba9e0ad053600cc6ac00a3fe
victoriavilleIT <83372520+victoriavilleIT@users.noreply.github.com>
2021-09-16 09:50:22 -0400

    Update SAPInstall.bat

--- SAPInstall.bat
+++ SAPInstall.bat
@@ -1,7 +1,7 @@
 mkdir c:\temp
-powershell.exe -command "Invoke-WebRequest https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/DownloadSAP.ps1 -Outfile C:\Temp\sap.ps1"
+powershell.exe -command "Invoke-WebRequest https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/DownloadSAP.ps1 -Outfile C:\Temp\DownloadSAP.ps1"
 ping -n 60 127.0.0.1 >NUL
 powershell.exe -command "Invoke-WebRequest https://raw.githubusercontent.com/victoriavilleIT/Scripts/main/SAPInstall2.bat -Outfile C:\Temp\SAPInstall2.bat"
 ping -n 30 127.0.0.1 >NUL
-powershell.exe -executionpolicy bypass -file C:\Temp\sap.ps1
+powershell.exe -executionpolicy bypass -file C:\Temp\DownloadSAP.ps1
 
